b7ced5cc605e5bb0146688e0bf3ca6f1b86c5f82
Francis Verissimo <francissv97@gmail.com>
2022-08-11 21:52:59 -0400

    add merge function inMemoryCache Page field

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,5 +2,13 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
   uri: "https://graphql.anilist.co",
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Page: {
+        merge(existing, incoming, { mergeObjects }) {
+          return mergeObjects(existing, incoming);
+        },
+      },
+    },
+  }),
 });
