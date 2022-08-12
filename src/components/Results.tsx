2e013e35c28a3a6784c521395c2b80d98444503c
Francis Verissimo <francissv97@gmail.com>
2022-08-11 21:58:14 -0400

    css tweaks

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,12 +1,12 @@
 import { GetAnimeInfoQueryResponse } from "../types/handleTypes";
 import { Loading } from "./Loading";
 
-interface ResultsType {
+interface Props {
   title: string;
   data: GetAnimeInfoQueryResponse | undefined;
 }
 
-export function Results({ title, data }: ResultsType) {
+export function Results({ title, data }: Props) {
   const animes = data && data.Page.media;
 
   return (
@@ -14,32 +14,30 @@ export function Results({ title, data }: ResultsType) {
       {!animes ? (
         <Loading />
       ) : (
-        <div className="px-2 max-w-screen-lg mx-auto shadow-2xl rounded pb-5 mb-10">
+        <div className="px-2 max-w-screen-xl mx-auto shadow-2xl rounded pb-5 mb-10">
           <div className="flex justify-between">
             <h2 className="my-4 text-zinc-600 text-md font-mainCondensed font-medium uppercase">
               {title}
             </h2>
 
-            <button className="hidden uppercase bg-gray-600 text-gray-200 px-2 h-fit my-auto rounded text-sm font-medium hover:text-main duration-200">
-              ver tudo
+            <button className="uppercase bg-gray-600 text-gray-200 px-2 h-fit my-auto rounded text-sm font-medium hover:text-main duration-300">
+              {"view all"}
             </button>
           </div>
 
-          <div className="grid gap-x-4 gap-y-5 justify-items-center grid-cols-[repeat(auto-fill,minmax(160px,1fr))] max-w-4xl mx-auto">
-            {animes.map((anime) => {
-              return (
-                <div key={anime.id} className="w-full group cursor-pointer">
-                  <img
-                    src={anime.coverImage.large}
-                    alt={`${anime.title}`}
-                    className="rounded-md group-hover:scale-105 w-full h-5/6 object-cover object-center mb-2 transition-all"
-                  />
-                  <p className="text-sm text-zinc-600 truncate transition-all group-hover:text-main">
-                    {anime.title.romaji}
-                  </p>
-                </div>
-              );
-            })}
+          <div className="grid gap-x-3 gap-y-5 justify-items-center grid-cols-2 sm:grid-cols-[repeat(auto-fill,minmax(190px,1fr))] max-w-5xl mx-auto">
+            {animes.map(({ id, title, coverImage }) => (
+              <div key={id} className="w-full group duration-200">
+                <img
+                  src={coverImage.large}
+                  alt={`${title}`}
+                  className="rounded-md group-hover:scale-105 w-full h-[90%] object-cover object-center mb-2 duration-300"
+                />
+                <p className="text-sm text-zinc-600 truncate group-hover:text-main duration-300">
+                  {title.romaji}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       )}
