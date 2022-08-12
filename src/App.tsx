0584025b978eeca166f8363888d717b45a69dca4
Francis Verissimo <francissv97@gmail.com>
2022-08-11 21:55:36 -0400

    use Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
-import { Header } from "./components/Header";
-import { TrendingNow } from "./components/TrendingNow";
-import { PopularThisSeason } from "./components/PopularThisSeason";
-import { UpcomingNextSeason } from "./components/UpcomingNextSeason";
-import { AllTimePopular } from "./components/AllTimePopular";
-import { Footer } from "./components/Footer";
-import {
-  currentSeason,
-  currentYear,
-  nextSeason,
-  nextSeasonYear,
-  perPage,
-} from "./utils/handleVariablesQueries";
+import { Home } from "./pages/Home";
 
 export function App() {
   return (
-    <div>
-      <Header />
-      <TrendingNow perPage={perPage} />
-      <PopularThisSeason
-        currentSeason={currentSeason()}
-        currentYear={currentYear()}
-        perPage={perPage}
-      />
-      <UpcomingNextSeason
-        nextSeason={nextSeason()}
-        nextSeasonYear={nextSeasonYear()}
-        perPage={perPage}
-      />
-      <AllTimePopular perPage={perPage} />
-      <Footer />
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
