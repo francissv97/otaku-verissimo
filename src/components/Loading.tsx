d24c16ac1af60e26d1ac37bb1a66defa62404a9c
francissverissimo <francissv97@gmail.com>
2023-03-22 13:28:02 -0400

    feat: create horizontal loading

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,13 +1,13 @@
-import { Fade, Grow } from "@mui/material";
+import { Fade, Grow, Zoom } from "@mui/material";
 import { CircleNotch } from "phosphor-react";
 
 export function CircularLoading() {
   return (
-    <Fade in timeout={800}>
-      <div className="fixed top-0 flex justify-center items-center h-full w-full">
-        <CircleNotch className="w-28 h-28 text-main/80 animate-spin" />
+    <Zoom in timeout={300}>
+      <div className="fixed z-20 top-0 flex justify-center items-center h-full w-full">
+        <CircleNotch weight="light" className="w-24 h-24 text-main/80 animate-spin border-2 border-zinc-200 rounded-full bg-zinc-50" />
       </div>
-    </Fade>
+    </Zoom>
   );
 }
 
@@ -16,10 +16,24 @@ export function CardSkeleton() {
     <>
       {Array.from({ length: 6 }, (v, k) => k).map((item) => (
         <Grow in timeout={400} key={item}>
-          <div className="cursor-pointer flex flex-col gap-1 animate-pulse">
+          <div className="flex flex-col gap-1 animate-pulse">
             <div className="h-52 md:h-60 bg-zinc-400 rounded" />
 
             <span className="h-[10%] bg-zinc-400 rounded"></span>
+          </div>
+        </Grow>
+      ))}
+    </>
+  );
+}
+
+export function HorizontalCardSkeleton() {
+  return (
+    <>
+      {Array.from({ length: 2 }, (v, k) => k).map((item) => (
+        <Grow in timeout={400} key={item}>
+          <div className="flex flex-col gap-1 animate-pulse">
+            <div className="h-32 bg-zinc-400 rounded" />
           </div>
         </Grow>
       ))}
