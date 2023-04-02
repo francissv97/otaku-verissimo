a61dcec350da74666880a756a2a85ef2e3b6beaf
francissverissimo <francissv97@gmail.com>
2023-04-01 23:38:54 -0400

    feat: add SimpleHeader component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { ViewAllParams } from "../types";
 import logo from "../assets/logo.svg";
-
-type HeaderResultProps = {
-  title: string;
-  paramViewAll?: ViewAllParams;
-};
+import { X } from "phosphor-react";
 
 export function Header() {
   return (
@@ -20,6 +16,11 @@ export function Header() {
     </header>
   );
 }
+
+type HeaderResultProps = {
+  title: string;
+  paramViewAll?: ViewAllParams;
+};
 
 export function HeaderResults({ title, paramViewAll }: HeaderResultProps) {
   const navigate = useNavigate();
@@ -36,6 +37,32 @@ export function HeaderResults({ title, paramViewAll }: HeaderResultProps) {
       >
         view all
       </button> */}
+    </div>
+  );
+}
+
+export function SimpleHeader() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="sm:block fixed z-30 bg-zinc-800/60 md:hover:bg-zinc-800 backdrop-blur-sm left-0 right-0 top-0 duration-300">
+      <div className="group flex w-full justify-between items-center max-w-6xl mx-auto px-4">
+        <div
+          className="p-2 cursor-pointer transition hover:bg-main/10"
+          onClick={() => navigate(-1)}
+        >
+          <X size={22} className="text-main" />
+        </div>
+
+        <div className="flex flex-1 justify-center">
+          <img
+            src={logo}
+            alt="otakuVERISSIMOlogo"
+            className="w-48 md:w-60 my-auto px-2 cursor-pointer"
+            onClick={() => navigate("/")}
+          />
+        </div>
+      </div>
     </div>
   );
 }
