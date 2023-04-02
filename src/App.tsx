3279cfb5ccdc98c53f93259a88667c63e40ed2da
francissverissimo <francissv97@gmail.com>
2023-04-01 22:44:00 -0400

    feat: add Staff/Character routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Anime } from "./pages/Anime";
+import { Character } from "./pages/Character";
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
+import { Staff } from "./pages/Staff";
 
 export function App() {
   return (
@@ -9,6 +11,9 @@ export function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/anime/:id" element={<Anime />} />
+        {/* <Route path="/manga/:id" element={<Manga />} /> */}
+        <Route path="/staff/:id" element={<Staff />} />
+        <Route path="/character/:id" element={<Character />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
