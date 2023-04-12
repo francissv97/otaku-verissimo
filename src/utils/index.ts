528736282c896548ea5874088e76e5ce9fa9dcc9
francissverissimo <francissv97@gmail.com>
2023-04-12 11:57:53 -0400

    refact: refatorar alguns métodos

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,41 +1,98 @@
-import { Location } from "react-router-dom";
+type StaffMediaRoleInitialType = {
+  node: {
+    id: number;
+    title: {
+      romaji: string;
+    };
+    coverImage: {
+      large: string;
+    };
+    startDate: {
+      day: number | null;
+      month: number | null;
+      year: number | null;
+    };
+  };
+  staffRole: string;
+};
 
-export function handleNavLocationStateFrom(location: Location) {
-  if (!location.state?.from) return [location.pathname];
+type StaffMediaRolesGrouped = {
+  node: {
+    id: number;
+    title: {
+      romaji: string;
+    };
+    coverImage: {
+      large: string;
+    };
+    startDate: {
+      day: number | null;
+      month: number | null;
+      year: number | null;
+    };
+  };
+  staffRoles: string[];
+};
 
-  if (location.state?.from.length > 0)
-    return [...location.state.from, location.pathname];
+export function groupStaffRolesByMedia(
+  staffMediaRoleArrayInitial: StaffMediaRoleInitialType[]
+): StaffMediaRolesGrouped[] {
+  const grouped: { [key: number]: StaffMediaRolesGrouped } = {};
+
+  staffMediaRoleArrayInitial.forEach((item) => {
+    const id = item.node.id;
+    const staffRole = item.staffRole;
+
+    if (!grouped[id]) {
+      grouped[id] = { staffRoles: [staffRole], node: { ...item.node } };
+    } else {
+      grouped[id].staffRoles.push(staffRole);
+    }
+  });
+
+  return Object.values(grouped);
+}
+
+export function sortStaffMediaRolesByStartDate(
+  staffMediaRolesGrouped: StaffMediaRolesGrouped[]
+): StaffMediaRolesGrouped[] {
+  return staffMediaRolesGrouped.sort((a, b) => {
+    const aStartDate = a.node.startDate;
+    const bStartDate = b.node.startDate;
+
+    if (!aStartDate || !bStartDate) {
+      return 0;
+    }
+
+    const aYear = aStartDate.year ?? Number.MAX_VALUE;
+    const bYear = bStartDate.year ?? Number.MAX_VALUE;
+    if (aYear !== bYear) {
+      return bYear - aYear;
+    }
+
+    const aMonth = aStartDate.month ?? Number.MAX_VALUE;
+    const bMonth = bStartDate.month ?? Number.MAX_VALUE;
+    if (aMonth !== bMonth) {
+      return bMonth - aMonth;
+    }
+
+    const aDay = aStartDate.day ?? Number.MAX_VALUE;
+    const bDay = bStartDate.day ?? Number.MAX_VALUE;
+    if (aDay !== bDay) {
+      return bDay - aDay;
+    }
+
+    return 0;
+  });
 }
 
 export function currentSeason() {
   const currentMonth = new Date().getMonth();
 
-  switch (currentMonth) {
-    case 0:
-      return "WINTER";
-    case 1:
-      return "WINTER";
-    case 2:
-      return "WINTER";
-    case 3:
-      return "SPRING";
-    case 4:
-      return "SPRING";
-    case 5:
-      return "SPRING";
-    case 6:
-      return "SUMMER";
-    case 7:
-      return "SUMMER";
-    case 8:
-      return "SUMMER";
-    case 9:
-      return "FALL";
-    case 10:
-      return "FALL";
-    default:
-      return "FALL";
-  }
+  if ([0, 1, 2].includes(currentMonth)) return "WINTER";
+  if ([3, 4, 5].includes(currentMonth)) return "SPRING";
+  if ([6, 7, 8].includes(currentMonth)) return "SUMMER";
+  return "FALL";
 }
 
 export function currentYear() {
@@ -43,16 +100,13 @@ export function currentYear() {
 }
 
 export function nextSeason() {
-  switch (currentSeason()) {
-    case "WINTER":
-      return "SPRING";
-    case "SPRING":
-      return "SUMMER";
-    case "SUMMER":
-      return "FALL";
-    case "FALL":
-      return "WINTER";
-  }
+  const seasonWeAreIn = currentSeason();
+  const seasons = ["WINTER", "SPRING", "SUMMER", "FALL"];
+  const index = seasons.findIndex((season) => season == seasonWeAreIn);
+
+  if (index == 3) return seasons[0];
+
+  return seasons[index + 1];
 }
 
 export function nextSeasonYear() {
