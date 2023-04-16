5ee1afc2951fb25bd52b1076d67bf07b4da613bb
francissverissimo <francissv97@gmail.com>
2023-04-16 00:55:22 -0400

    chore: remove external link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,16 +5,9 @@ export function Footer() {
     <footer className="flex flex-col items-center max-w-6xl w-full sm:flex-row sm:justify-between gap-2 mx-auto p-4">
       <img src={logo} alt="otakuVERISSIMOlogo" className="w-48 opacity-75" />
 
-      <div className="flex flex-col gap-1">
-        <p className="text-zinc-700 text-sm">
-          <span className="mr-1">Powered by</span>
-          <a
-            href="https://anilist.co/"
-            target="_blank"
-            className="text-zinc-100 bg-zinc-500 hover:bg-zinc-600 font-semibold inline-block px-1 rounded duration-200"
-          >
-            Anilist
-          </a>
+      <div className="flex flex-col gap-1 items-center md:items-end">
+        <p className="text-zinc-600 text-sm font-medium">
+          <span>Powered by Anilist</span>
         </p>
 
         <p className="text-zinc-700 text-sm">
