e7b3dc645f74a3be1d3edbdf3ae02c452685162f
francissverissimo <francissv97@gmail.com>
2024-01-24 10:55:21 -0400

    feat: check viewer data if needed, use useAuth

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,28 @@ import { AnimeHome } from "./pages/animeHome";
 import { NotFound } from "./components/NotFound";
 import { Staff } from "./pages/staff";
 import { Search } from "./pages/search";
+import { Profile } from "./pages/profile";
+import { useEffect } from "react";
+import { useViewer } from "./hooks/useViewer";
+import { useAuth } from "./hooks/useAuth";
 
 export function App() {
+  const { signin } = useAuth();
+  
+  async function checkViewerDataIfNeeded() {
+    const { refreshViewerDataIfNeeded } = useViewer();
+
+    const viewer = await refreshViewerDataIfNeeded();
+
+    if (!viewer) return;
+
+    signin(viewer);
+  }
+
+  useEffect(() => {
+    checkViewerDataIfNeeded();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,6 +36,7 @@ export function App() {
         <Route path="/character/:id" element={<Character />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/search" element={<Search />} />
+        <Route path="/profile" element={<Profile />} />
         {/* <Route path="/manga/:id" element={<MangaHome />} /> */}
       </Routes>
     </BrowserRouter>
