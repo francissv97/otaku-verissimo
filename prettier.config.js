22d7666e32a993c5cf4eafa17b92473b434351b1
francissverissimo <francissv97@gmail.com>
2024-03-24 17:34:15 -0400

    chore: rm packages, use prettier.config.js file pacjages: @emotion/react, @emotion/styled, @mui/material

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 100,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
+
+export default config
