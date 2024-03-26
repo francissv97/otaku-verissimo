32d960dda27bc1924868c3fe16cad4a321f069b0
francissverissimo <francissv97@gmail.com>
2024-03-25 23:19:50 -0400

    feat: add mediaListOptions field to type TViewer

--- src/hooks/use-viewer.ts
+++ src/hooks/use-viewer.ts
@@ -1,5 +1,5 @@
-import { useToken } from "./use-token";
-import { TStoredViewer } from "@/types/t-viewer";
+import { useToken } from './use-token'
+import { TStoredViewer } from '@/types/t-viewer'
 
 const GET_VIEWER = `
   query ViewerQuery {
@@ -9,75 +9,78 @@ const GET_VIEWER = `
       avatar {
         medium
       }
+      mediaListOptions {
+        animeList {
+          sectionOrder
+        }
+      }
     }
   }
-`;
+`
 
 export function useViewer() {
-  const { getURLToken, getStoredToken } = useToken();
+  const { getURLToken, getStoredToken } = useToken()
 
-  async function fetchViewerData(
-    token: string,
-  ): Promise<TStoredViewer | undefined> {
+  async function fetchViewerData(token: string): Promise<TStoredViewer | undefined> {
     try {
-      const response = await fetch("https://graphql.anilist.co", {
-        method: "POST",
+      const response = await fetch('https://graphql.anilist.co', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
           authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({ query: GET_VIEWER }),
-      });
+      })
 
       if (!response.ok) {
-        throw new Error(`Error on request Viewer: ${response.status}`);
+        throw new Error(`Error on request Viewer: ${response.status}`)
       }
 
-      const data = await response.json();
+      const data = await response.json()
       const newViewer = {
         ...data.data.Viewer,
         lastAccess: Date.now(),
-      } as TStoredViewer;
+      } as TStoredViewer
 
-      localStorage.setItem("viewer", JSON.stringify(newViewer));
+      localStorage.setItem('viewer', JSON.stringify(newViewer))
 
-      return newViewer;
+      return newViewer
     } catch (error) {
-      console.error("Error on request Viewer:", error);
-      return undefined;
+      console.error('Error on request Viewer:', error)
+      return undefined
     }
   }
 
-  async function refreshViewerDataIfNeeded(): Promise<
-    TStoredViewer | undefined
-  > {
-    const urlToken = getURLToken();
+  async function refreshViewerDataIfNeeded(): Promise<TStoredViewer | undefined> {
+    const urlToken = getURLToken()
 
     if (!urlToken) {
-      const accessToken = getStoredToken();
-      const storedViewer = getStoredViewer();
+      const accessToken = getStoredToken()
+      const storedViewer = getStoredViewer()
 
-      if (!accessToken || !storedViewer) return undefined;
+      if (!accessToken || !storedViewer) return undefined
 
-      const timestampDifference = Date.now() - storedViewer.lastAccess;
-      const oneHourInMs = 60 * 60 * 1000;
+      const timestampDifference = Date.now() - storedViewer.lastAccess
+      const oneHourInMs = 60 * 60 * 1000
 
-      return timestampDifference > oneHourInMs
-        ? await fetchViewerData(accessToken)
-        : storedViewer;
+      if (timestampDifference > oneHourInMs || !storedViewer) {
+        return await fetchViewerData(accessToken)
+      }
+
+      return storedViewer
     }
 
-    localStorage.setItem("access_token", urlToken);
-    return await fetchViewerData(urlToken);
+    localStorage.setItem('access_token', urlToken)
+    return await fetchViewerData(urlToken)
   }
 
   function getStoredViewer(): TStoredViewer | null {
-    const storedViewer = localStorage.getItem("viewer");
-    return storedViewer ? JSON.parse(storedViewer) : null;
+    const storedViewer = localStorage.getItem('viewer')
+    return storedViewer ? JSON.parse(storedViewer) : null
   }
 
   return {
     getStoredViewer,
     refreshViewerDataIfNeeded,
-  };
+  }
 }
--- src/types/t-viewer.ts
+++ src/types/t-viewer.ts
@@ -1,9 +1,14 @@
 export type TViewer = {
-  id: number;
-  name: string;
-  avatar: { medium: string };
-};
+  id: number
+  name: string
+  avatar: { medium: string }
+  mediaListOptions: {
+    animeList: {
+      sectionOrder: Array<string>
+    }
+  }
+}
 
 export type TStoredViewer = TViewer & {
-  lastAccess: number /** Date.now() */;
-};
+  lastAccess: number /** Date.now() */
+}
