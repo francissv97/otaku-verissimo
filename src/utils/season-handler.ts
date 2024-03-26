78c964de7f1e974812789bf524d99e9eff330f6c
francissverissimo <francissv97@gmail.com>
2024-03-25 22:54:49 -0400

    refact: create utility that handles seasons

---
+++ src/utils/season-handler.ts
@@ -0,0 +1,28 @@
+class SeasonHandler {
+  getCurrentSeason(): string {
+    const currentMonth = new Date().getMonth()
+    if ([0, 1, 2].includes(currentMonth)) return 'WINTER'
+    if ([3, 4, 5].includes(currentMonth)) return 'SPRING'
+    if ([6, 7, 8].includes(currentMonth)) return 'SUMMER'
+    return 'FALL'
+  }
+
+  getNextSeason(): string {
+    const seasonWeAreIn = this.getCurrentSeason()
+    const seasons = ['WINTER', 'SPRING', 'SUMMER', 'FALL']
+    const index = seasons.findIndex((season) => season == seasonWeAreIn)
+    if (index === 3) return seasons[0]
+    return seasons[index + 1]
+  }
+
+  getCurrentYear(): number {
+    return new Date().getFullYear()
+  }
+
+  getNextYear(): number {
+    if (this.getNextSeason() === 'WINTER') return this.getCurrentYear() + 1
+    return this.getCurrentYear()
+  }
+}
+
+export const seasonHandler = new SeasonHandler()
