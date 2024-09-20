9ed6123e6af0f7c23bbdf4f89da8aa68fa19addf
francissverissimo <francissv97@gmail.com>
2024-09-19 21:52:36 -0400

    refactor: update apollo client configuration with environment token

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,22 +1,16 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
 
-const httpLink = createHttpLink({
+const token = import.meta.env.VITE_TOKEN
+
+const link = createHttpLink({
   uri: 'https://graphql.anilist.co',
-})
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('access_token')
-
-  return {
-    headers: {
-      ...headers,
-      ...(token && { authorization: `Bearer ${token}` }),
-    },
-  }
+  credentials: 'same-origin',
+  headers: {
+    ...(token && { authorization: `Bearer ${token}` }),
+  },
 })
 
 export const client = new ApolloClient({
-  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
+  link,
 })
