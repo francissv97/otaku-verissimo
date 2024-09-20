96dda7517e41d4cfe369af8760161c11d09aacc1
francissverissimo <francissv97@gmail.com>
2024-09-20 00:10:01 -0400

    style: use single quotes and no semicolons

--- src/utils/staff-media-roles.ts
+++ src/utils/staff-media-roles.ts
@@ -1,89 +1,92 @@
 type StaffMediaRoleInitialType = {
   node: {
-    id: number;
+    id: number
     title: {
-      romaji: string;
-    };
+      userPreferred: string
+    }
     coverImage: {
-      large: string;
-    };
+      large: string
+    }
     startDate: {
-      day: number | null;
-      month: number | null;
-      year: number | null;
-    };
-    type: "ANIME" | "MANGA";
-  };
-  staffRole: string;
-};
+      day: number | null
+      month: number | null
+      year: number | null
+    }
+    type: 'ANIME' | 'MANGA'
+  }
+  staffRole: string
+}
 
 type StaffMediaRolesGrouped = {
   node: {
-    id: number;
+    id: number
     title: {
-      romaji: string;
-    };
+      userPreferred: string
+    }
     coverImage: {
-      large: string;
-    };
+      large: string
+    }
     startDate: {
-      day: number | null;
-      month: number | null;
-      year: number | null;
-    };
-    type: "ANIME" | "MANGA";
-  };
-  staffRoles: string[];
-};
+      day: number | null
+      month: number | null
+      year: number | null
+    }
+    type: 'ANIME' | 'MANGA'
+  }
+  staffRoles: string[]
+}
 
 class StaffMediaRolesUtils {
   group(data: StaffMediaRoleInitialType[]): StaffMediaRolesGrouped[] {
-    const grouped: { [key: number]: StaffMediaRolesGrouped } = {};
+    const grouped: { [key: number]: StaffMediaRolesGrouped } = {}
 
     data.forEach((item) => {
-      const id = item.node.id;
-      const staffRole = item.staffRole;
+      const id = item.node.id
+      const staffRole = item.staffRole
 
       if (!grouped[id]) {
-        grouped[id] = { staffRoles: [staffRole], node: { ...item.node } };
+        grouped[id] = { staffRoles: [staffRole], node: { ...item.node } }
       } else {
-        grouped[id].staffRoles.push(staffRole);
+        grouped[id].staffRoles.push(staffRole)
       }
-    });
+    })
 
-    return Object.values(grouped);
+    return Object.values(grouped)
   }
 
   sort(data: StaffMediaRolesGrouped[]): StaffMediaRolesGrouped[] {
     return data.sort((a, b) => {
-      const aStartDate = a.node.startDate;
-      const bStartDate = b.node.startDate;
+      const aStartDate = a.node.startDate
+      const bStartDate = b.node.startDate
 
       if (!aStartDate || !bStartDate) {
-        return 0;
+        return 0
       }
 
-      const aYear = aStartDate.year ?? Number.MAX_VALUE;
-      const bYear = bStartDate.year ?? Number.MAX_VALUE;
+      const aYear = aStartDate.year ?? Number.MAX_VALUE
+      const bYear = bStartDate.year ?? Number.MAX_VALUE
+
       if (aYear !== bYear) {
-        return bYear - aYear;
+        return bYear - aYear
       }
 
-      const aMonth = aStartDate.month ?? Number.MAX_VALUE;
-      const bMonth = bStartDate.month ?? Number.MAX_VALUE;
+      const aMonth = aStartDate.month ?? Number.MAX_VALUE
+      const bMonth = bStartDate.month ?? Number.MAX_VALUE
+
       if (aMonth !== bMonth) {
-        return bMonth - aMonth;
+        return bMonth - aMonth
       }
 
-      const aDay = aStartDate.day ?? Number.MAX_VALUE;
-      const bDay = bStartDate.day ?? Number.MAX_VALUE;
+      const aDay = aStartDate.day ?? Number.MAX_VALUE
+      const bDay = bStartDate.day ?? Number.MAX_VALUE
+      
       if (aDay !== bDay) {
-        return bDay - aDay;
+        return bDay - aDay
       }
 
-      return 0;
-    });
+      return 0
+    })
   }
 }
 
-export const staffMediaRolesUtils = new StaffMediaRolesUtils();
+export const staffMediaRolesUtils = new StaffMediaRolesUtils()
