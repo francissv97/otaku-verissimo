ec9705b312c70b49aa74f7995bedb01634f1cdbf
francissverissimo <francissv97@gmail.com>
2024-09-24 00:04:16 -0400

    style: hidde MagnifyingGlass button; css tweaks

--- src/pages/dashboard/tabs/anime-list/anime-list-header.tsx
+++ src/pages/dashboard/tabs/anime-list/anime-list-header.tsx
@@ -1,24 +1,23 @@
-import { MagnifyingGlass } from "@phosphor-icons/react";
+import { MagnifyingGlass } from '@phosphor-icons/react'
 
 export function AnimeListHeader() {
   return (
-    <div className="md:ml-14 fixed left-0 right-0 top-0 z-30 h-14 bg-zinc-950/50 backdrop-blur sm:block"
-    >
-      <div className="group mx-auto flex h-full w-full max-w-5xl items-center justify-between px-4">
+    <div className="fixed left-0 right-0 top-0 z-30 h-14 bg-zinc-950/50 px-4 backdrop-blur sm:block md:ml-14">
+      <div className="group mx-auto flex h-full w-full max-w-5xl items-center justify-between">
         <div className="flex items-center gap-4">
-          <span className="font-sans text-2xl text-main">Anime List</span>
+          <span className="text-xl font-medium uppercase text-main">anime list</span>
         </div>
 
-        <div className="flex items-center gap-2">
-          <div className="flex h-14 w-14 cursor-pointer items-center justify-center hover:bg-white/5">
+        <div className="flex items-center gap-2 opacity-0">
+          <div className="group flex h-14 w-14 cursor-pointer items-center justify-center">
             <MagnifyingGlass
               size={24}
               weight="bold"
-              className="text-zinc-400"
+              className="group text-zinc-400 group-hover:text-zinc-200"
             />
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 }
